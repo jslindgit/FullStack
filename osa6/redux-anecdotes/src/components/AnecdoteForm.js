52aea90903d1d4bd3755f1297f3afe0ba68d2ea1
Joonas Lindstedt <joonas@helsinginlipputehdas.fi>
2023-01-17 17:12:53 +0200

    Tehtävät -6.14. valmiit

--- osa6/redux-anecdotes/src/components/AnecdoteForm.js
+++ osa6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,15 +1,17 @@
 import { useDispatch } from 'react-redux'
 import { newAnecdote } from '../reducers/anecdoteReducer'
 import { setMessage } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
 	const dispatch = useDispatch()
 
-	const addAnecdote = (event) => {
+	const addAnecdote = async (event) => {
 		event.preventDefault()
 		const content = event.target.anecdote.value
 		event.target.anecdote.value = ''
-		dispatch(newAnecdote(content))
+		const returnedAnecdote = await anecdoteService.createNew(content)
+		dispatch(newAnecdote(returnedAnecdote))
 		dispatch(setMessage('You added a new anecdote: \'' + content + '\''))
 	}
 
