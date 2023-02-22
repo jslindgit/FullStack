bc4e2640c34b0d79b33508e915c8bc01d92ed80c
Joonas Lindstedt <68861164+jslindgit@users.noreply.github.com>
2023-02-23 00:06:25 +0200

    9.3. valmis

--- osa9/9_1-9_7/bmiCalculator.ts
+++ osa9/9_1-9_7/bmiCalculator.ts
@@ -1,29 +1,58 @@
-const calculateBmi = (heightCm: number, weightKg: number): string => {	
-	const bmi = weightKg / Math.pow(heightCm / 100, 2)
-	if (bmi <= 16) {
-		return 'Underweight (Severe thinness)'
-	}
-	else if (bmi <= 16.95) {
-		return 'Underweight (Moderate thinness)'
-	}
-	else if (bmi <= 18.45) {
-		return 'Underweight (Mild thinness)'
-	}
-	else if (bmi <= 24.95) {
-		return 'Normal (healthy weight)'
-	}
-	else if (bmi <= 29.95) {
-		return 'Overweight (Pre-obese)'
-	}
-	else if (bmi <= 34.95) {
-		return 'Obese (Class I)'
-	}
-	else if (bmi <= 39.95) {
-		return 'Obese (Class II)'
+interface BmiValues {
+	heightCm: number;
+	weightKg: number;
+}
+
+const parsedArguments = (args: string[]): BmiValues => {
+	if (args.length < 4) throw new Error('Not enough parameters');
+	if (args.length > 4) throw new Error('Too many parameters');
+
+	if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+		return {
+			heightCm: Number(args[2]),
+			weightKg: Number(args[3])
+		}
 	}
 	else {
-		return 'Obese (Class III)'
+		throw new Error('Provided values are not numbers!');
 	}
 }
 
-console.log(calculateBmi(180, 74))
+const calculateBmi = (heightCm: number, weightKg: number): string => {	
+	const bmi = weightKg / Math.pow(heightCm / 100, 2);
+	if (bmi <= 16) {
+		return 'Underweight (Severe thinness)';
+	}
+	else if (bmi <= 16.95) {
+		return 'Underweight (Moderate thinness)';
+	}
+	else if (bmi <= 18.45) {
+		return 'Underweight (Mild thinness)';
+	}
+	else if (bmi <= 24.95) {
+		return 'Normal (healthy weight)';
+	}
+	else if (bmi <= 29.95) {
+		return 'Overweight (Pre-obese)';
+	}
+	else if (bmi <= 34.95) {
+		return 'Obese (Class I)';
+	}
+	else if (bmi <= 39.95) {
+		return 'Obese (Class II)';
+	}
+	else {
+		return 'Obese (Class III)';
+	}
+}
+
+try {
+	const { heightCm, weightKg } = parsedArguments(process.argv)
+	console.log(calculateBmi(heightCm, weightKg))
+} catch (error: unknown) {
+	let errorMessage = 'Something bad happened.';
+	if (error instanceof Error) {
+		errorMessage += ' Error: ' + error.message;
+	}
+	console.log(errorMessage);
+}
---
+++ osa9/9_1-9_7/exerciseCalculator.ts
@@ -0,0 +1,80 @@
+interface ExerciseResult {
+    periodLength: number;
+    trainingDays: number;
+    success: boolean;
+    rating: number;
+    ratingDescription: string;
+    target: number;
+    average: number;
+}
+
+interface ExerciseValues {
+    target: number;
+    hours: number[];    
+}
+
+const parsedArgs = (args: string[]): ExerciseValues => {
+    if (args.length < 4) throw new Error('Not enough parameters.');
+
+    let target: number;
+    let hours: number[] = [];
+    
+    args.forEach((value, index) => {
+        if (index < 2) return;
+        if (isNaN(Number(value))) {
+            throw new Error('Parameter "' + value + '" is not a number!');
+        }
+        if (index === 0) { 
+            target = Number(value);
+        } else {
+            hours.push(Number(value));
+        }
+    })
+
+    return {        
+        target: target,
+        hours: hours
+    }
+}
+
+const calculateExercises = (hours: Array<number>, target: number): ExerciseResult => {
+    const average = hours.reduce((partial, a) => partial + a, 0) / hours.length
+    let rating: number;
+    if (average < target * 0.8) {
+        rating = 1;
+    }
+    else {
+        rating = average < target ? 2 : 3;
+    }    
+    let desc: string;
+    if (rating == 1) {
+        desc = 'Poor';
+    }
+    else if (rating == 2) {
+        desc = 'Ok';
+    }
+    else {
+        desc = 'Good';
+    }
+    return {
+        periodLength: hours.length,
+        trainingDays: hours.filter(h => h > 0).length,
+        success: average >= target,
+        rating: rating,
+        ratingDescription: desc,
+        target: target,
+        average: average
+    }
+}
+
+try {
+    const { target, hours } = parsedArgs(process.argv);
+    console.log(calculateExercises(hours, target));
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened.';
+    if (error instanceof Error) {
+        errorMessage += ' Error: ' + error.message;        
+    }
+    console.log(errorMessage)
+}
+//console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
