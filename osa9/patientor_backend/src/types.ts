ca6e2b73c9bf7a32443289b20e657f4395f14a3a
Joonas Lindstedt <joonas@helsinginlipputehdas.fi>
2023-03-03 16:37:59 +0200

    9.22 valmis

--- osa9/patientor_backend/src/types.ts
+++ osa9/patientor_backend/src/types.ts
@@ -4,14 +4,51 @@ export enum Gender {
 	Other = 'other'
 }
 
+export enum HealthCheckRating {
+	"Healthy" = 0,
+	"LowRisk" = 1,
+	"HighRisk" = 2,
+	"CriticalRisk" = 3
+}
+
 export interface Diagnose {
 	code: string;
 	name: string;
 	latin?: string;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+interface BaseEntry {
+	id: string;
+	description: string;
+	date: string;
+	specialist: string;
+	diagnosisCodes?: Array<Diagnose['code']>;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+	type: "HealthCheck",
+	healthCheckRating: HealthCheckRating
+}
+
+export interface sickLeaveDates {
+	startDate: string,
+	endDate: string
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+	type: "OccupationalHealthcare",
+	employerName: string,
+	sickLeave?: sickLeaveDates
+}
+
+export interface dischargeDate {
+	date: string,
+	criteria: string
+}
+
+interface HospitalEntry extends BaseEntry {
+	type: "Hospital",
+	discharge: dischargeDate
 }
 
 export interface Patient {
@@ -26,3 +63,7 @@ export interface Patient {
 
 export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
 export type NewPatient = Omit<Patient, 'id'>;
+export type Entry =
+	| HospitalEntry
+	| OccupationalHealthcareEntry
+	| HealthCheckEntry;
